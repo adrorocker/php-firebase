8b7b636a6a48a30b51c5ef239f68a696d7ffdcbb
Alejandro Morelos <adro.skate@gmail.com>
2016-10-31 15:03:37 -0500

    Move code to the base client and add more tests

---
+++ tests/Clients/FakeGuzzle.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PHP-Firebase
+ *
+ * @link      https://github.com/adrorocker/php-firebase
+ * @copyright Copyright (c) 2016 Adro Rocker
+ * @author    Adro Rocker <alejandro.morelos@jarwebdev.com>
+ */
+namespace PhpFirebase\Clients;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
+
+class FakeGuzzle extends Client
+{
+    public function __construct(array $config = [])
+    {
+        $mock = new MockHandler([
+                        new Response(200, [],'Hello Firebase GET'),
+                        new Response(200, [],'Hello Firebase POST'),
+                        new Response(200, [],'Hello Firebase PUT'),
+                        new Response(200, [],'Hello Firebase PATCH'),
+                        new Response(200, [],'Hello Firebase DELETE'),
+                        new Response(202, ['Content-Length' => 0]),
+                        new RequestException("Error Communicating with Server", new Request('GET', 'test')),
+                    ]);
+
+        $handler = HandlerStack::create($mock);
+
+        parent::__construct(['handler' => $handler]);
+    }
+}
